908a431941372e4db9d4699c1130722597039ccb
Aiden Crowe <aiden.crowe@duke.edu>
2023-04-20 14:07:01 -0400

    Meow

--- ECE590/MoveServoAngles.py
+++ ECE590/MoveServoAngles.py
@@ -13,56 +13,51 @@ else:
 #servo 2 is 141.5 0
 #servo 3 is 90 18
 
-S1_HOME = 90
-S1_OFFSET = -13.5
-S1_LLIM = 30
-S1_RLIM = 160
+S1_HOME = 120
+S1_OFFSET = 0
+S1_LLIM = 105
+S1_RLIM = 206
 
-S2_HOME = 143
+S2_HOME = 168
 S2_OFFSET = 0
-S2_LLIM = 110
-S2_RLIM = 180
+S2_LLIM = 39
+S2_RLIM = 198
 
-S3_HOME = 109
+S3_HOME = 145
 S3_OFFSET = 0
-S3_LLIM = 45
-S3_RLIM = 170
+S3_LLIM = 70
+S3_RLIM = 225
 
-S4_HOME = 46.5
-S4_OFFSET = -30
-S4_LLIM = 18
-S4_RLIM = 82
+S4_HOME = 143
+S4_OFFSET = 0
+S4_LLIM = 47
+S4_RLIM = 153
 
-S5_HOME = 90
-S5_OFFSET = -6
-S5_LLIM = 25
-S5_RLIM = 150
+S5_HOME = 179
+S5_OFFSET = 0
+S5_LLIM = 47
+S5_RLIM = 210
 
-S6_HOME = 90
-S6_OFFSET = 18
-S6_LLIM = 60
-S6_RLIM = 130
+S6_HOME = 166
+S6_OFFSET = 0
+S6_LLIM = 90
+S6_RLIM = 222
 
-S7_HOME = 86.4
+S7_HOME = 195
 S7_OFFSET = 0
-S7_LLIM = 25
-S7_RLIM = 150
+S7_LLIM = 133
+S7_RLIM = 233
 
-S8_HOME = 117.5
-S8_OFFSET = 18
-S8_LLIM = 80
-S8_RLIM = 145
-
-SERVO_HOMES = [0, S1_HOME, S2_HOME, S3_HOME, S4_HOME, S5_HOME, S6_HOME, S7_HOME, S8_HOME]
-SERVO_OFFSETS = [0, S1_OFFSET, S2_OFFSET, S3_OFFSET, S4_OFFSET, S5_OFFSET, S6_OFFSET, S7_OFFSET, S8_OFFSET]
-SERVO_LOWER_LIMITS = [0, S1_LLIM, S2_LLIM, S3_LLIM, S4_LLIM, S5_LLIM, S6_LLIM, S7_LLIM, S8_LLIM]
-SERVO_UPPER_LIMITS = [0, S1_RLIM, S2_RLIM, S3_RLIM, S4_RLIM, S5_RLIM, S6_RLIM, S7_RLIM, S8_RLIM]
+SERVO_HOMES = [0, S1_HOME, S2_HOME, S3_HOME, S4_HOME, S5_HOME, S6_HOME, S7_HOME]
+SERVO_OFFSETS = [0, S1_OFFSET, S2_OFFSET, S3_OFFSET, S4_OFFSET, S5_OFFSET, S6_OFFSET, S7_OFFSET]
+SERVO_LOWER_LIMITS = [0, S1_LLIM, S2_LLIM, S3_LLIM, S4_LLIM, S5_LLIM, S6_LLIM, S7_LLIM]
+SERVO_UPPER_LIMITS = [0, S1_RLIM, S2_RLIM, S3_RLIM, S4_RLIM, S5_RLIM, S6_RLIM, S7_RLIM]
 
 
 def main():
     servos = []
     try:
-        for i in range(1, 9):
+        for i in range(1, 8):
             servos.append(LX16A(i))
             servos[i-1].set_angle_limits(SERVO_LOWER_LIMITS[i], SERVO_UPPER_LIMITS[i])
             servos[i-1].set_angle_offset(SERVO_OFFSETS[i])
@@ -93,7 +88,6 @@ def main():
         servos[4].move(float(row[5]), 50)
         servos[5].move(float(row[6]), 50)
         servos[6].move(float(row[7]), 50)
-        servos[7].move(float(row[8]), 50)
         
         time.sleep((int(row[0]))/1000)
 
